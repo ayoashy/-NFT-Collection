df0e5fd4a19175bda543c5d3833e2e0915f9d005
Topboyshoks <ashiruayo@gmail.com>
2023-04-23 14:19:56 +0100

    added picture folder

---
+++ my-app/my-app/src/pages/api/token/[token].js
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  const tokenId = req.query.token;
+  const name = `CryptoDev ${tokenId}`;
+  const description = 'CryptoDev is an NFT-Collection for Web3 developers';
+  const image = `https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/${
+    Number(tokenId) - 1
+  }.svg`;
+
+  res.status(200).json({ name, description, image });
+}
