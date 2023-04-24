6bb32ebb94ca1d7607ad1bc10650d88c479e29b4
Topboyshoks <ashiruayo@gmail.com>
2023-04-24 04:44:10 +0100

    metadata corrected

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = '0xd4FA46698c27Eabc8F195f782Bbf16FAE529b2db';
 
-const METADATA_URL =
-  'https://nft-collection-for-cryptodev.vercel.app/api/token/s';
+const METADATA_URL = `https://nft-collection-for-cryptodev.vercel.app/api/token/`;
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
